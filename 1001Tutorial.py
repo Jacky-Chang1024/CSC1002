8b4bcd74cce298c7e8b19ae966081908581dd9f5
Jacky-Chang1024 <120090315@link.cuhk.edu.cn>
2021-02-24 23:41:51 +0800

    A fulfiling day

--- 1001Tutorial.py
+++ 1001Tutorial.py
@@ -21,7 +21,10 @@ print(l)
 
  # Q1
 """
-a, b, c = map(float, input('Enter: ').split(','))
+a, b, c = map(float, input('Enter: ').split(',')) 
+
+#不过这里a,b,c=eval(input("。。。"))就好。。。无语
+
 print(a, b, c)
 if b**2 - 4*a*c > 0:    
     x1 = 1/(2*a) * (-b + (b**2 - 4*a*c)**0.5)
@@ -72,6 +75,23 @@ while (1):
 
  #Q4
 """ 
+
+#标答在这里用了
+##Give an initial value other than 0
+integer=None
+
+##if integer==0, stop the loop
+while integer != 0:       
+    ##  Use try except to capture the error input 
+    try:
+        integer=int(input("Enter an integer, the input ends if it is 0:"))
+    except:
+        print("Invalid number!Please input again!")
+        ##  Continue the loop while not execute the following code
+        continue
+    
+来避免输入不合法字符使程序崩溃
+
 (sum, p, n, i)= (0, 0, 0, 0)
 while (1):
     a = int(input('Enter an interger, the input ends if it is 0: '))
@@ -92,6 +112,8 @@ print('The average of numbers is %.2f'%avg)
 
  #Q5
 '''
+
+
 a = int(input())
 i = 1
 l = []
@@ -106,6 +128,33 @@ while (i):
 
 print(','.join(str(j) for j in l))
 #qPay attention to the requirment of output
+
+标答要好的多
+##Use try except to capture the error of not entering a number
+try:
+    integer=int(input("Enter an integer:"))
+except:
+    integer=None
+    
+if integer is None:
+    print("The input is invalid!")
+else:
+    ##  Start from 2
+    factor=2
+    ##  Iterate from 2 to the integer
+    while factor<=integer:             
+        if integer%factor==0:
+            ##  Stop when factor equal to the integer
+            if integer==factor:         
+                print(factor)
+                break
+            print(factor,end=',')
+            integer/=factor
+        else:
+            factor+=1
+
+#我只能说这个循环太妙了
+
 '''
 
 #Q6
@@ -120,5 +169,33 @@ for i in range(1,n+1):
     for j in range(2,i+1):
         print('%d '%j,end='')
     print('') 
+
+
+#标答用不等式和定位来做好得多
+for row in range(1,numOfLine+1):
+        
+    ##  Display the left part of the row
+    for column in range(numOfLine,1,-1):
+        if column<=row:
+            ##  Print the number
+            print("%4d"%column,end='')      
+        else:
+            ##  Print space
+            print("%4s"%" ",end='')
+            
+    ##  Display the right part of the row
+    for column in range(1,numOfLine+1):
+        if column<=row:
+            ##  Print the number
+            print("%4d"%column,end='')      
+        else:
+            ##  Print space
+            print("%4s"%" ",end='')
+            
+    ##  Change a new line to print
+    print()
+
+
+
 '''
 
---
+++ 1002Tutorial.py
@@ -0,0 +1,12 @@
+""" 
+l1 = (input('Please enter the first word:'))
+l2 = (input('Please enter the second word:'))
+
+l11 = list(l1)
+l22 = list(l2)
+l11.sort()
+l22.sort()
+if l11 == l22:
+    print('%s is an anagram of %s.'%(l1,l2)) 
+"""
+    
