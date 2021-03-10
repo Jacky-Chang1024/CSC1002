730623d4fa50223d52602826df2780a6cfb2f41e
Jacky-Chang1024 <120090315@link.cuhk.edu.cn>
2021-03-10 20:33:57 +0800

    Tutorail 5

--- 1002Tutorial.py
+++ 1002Tutorial.py
@@ -172,4 +172,55 @@ for i in range(N-2):
 print(l[i],l[j],l[k])
 
  '''
+ 
+ #TUT5
+
+# Build a function called add_money , client can use this function to put money into the
+# machine
+
+def add_money(p_money, p_balance):
+    g_money_list = [5,10,20,50,100]
+    if p_money in g_money_list: 
+        print
+    else:
+        print(" The machine only supports the money with 5, 10, 20, 50 or 100 face value. ")
+
+
+# Create a goods list and a price list as global variable with the same length as below
+
+goods_list = ['apple','orange','cola','juice','banana']
+price_list = [3, 4, 5, 6, 4]
+
+def show_goods():
+    global goods_list
+    global price_list
+    print('%s%10s' %('Goods', 'Price'))
+    for i in range(5):
+        print('%-10s%d'%(goods_list[i] + ':', price_list[i]))
+
+show_goods()
+
+def buy_goods(p_balance, p_name, p_number = 1):
+    global goods_list
+    global price_list
+
+    if p_name in goods_list:
+        p_unit_price = price_list[goods_list.index(p_name)] 
+        balance = p_balance - p_number * p_unit_price
+        if balance >= 0:
+            print('Buy successfully.')
+            return balance
+        else:
+            print('Your money is not enough.')
+            return p_balance
+    else:
+        print('No this term.')
+        return p_balance
+
+print('Your balance:', buy_goods(20,'appe',3))
+print('Your balance:', buy_goods(20,'apple',3))
+
+
+# 3. Reuse the functions in Q1 and Q2, then combine and invoke them to be a simple vending
+# machine.
 
